965bc3805684afc99dc71024894ee3833ae71ae5
Mimimimips <mipam.quici@ensta-bretagne.org>
2022-03-31 11:58:15 +0200

    V1 ecosystem (fonctions + début de remplissage fonctions)

--- ecosysteme.py
+++ ecosysteme.py
@@ -1,2 +1,63 @@
-fgeyvgyez
-mqlsssssssslfkj amf
+
+from numpy.random import randint
+
+class Ecosystem(list):
+    def __init__(self,nbvehic,nbturn,maxlength,maxwidth=4):
+
+        self.nbvehic=nbvehic
+        self.nbturn=nbturn
+        self.maxlength=maxlength
+        self.maxwidth=maxwidth
+
+
+
+    def __str__(self):
+        """
+        remodel the str function to show our road
+        :return:
+        """
+        return self.str2()
+
+    def str2(self):
+        """
+        function to create our endless road
+        :return:
+        """
+        r=""
+        for i in range (self.maxwidth+1):
+            for j in range (self.maxlength+1):
+                r+="."
+            r+="\n"
+            return r
+
+
+
+    def placement(self):
+        """
+        places the vehicles on the road
+        :return:
+        """
+        x=randint(0,self.maxwidth)
+        y=randint(0,self.maxlength)
+        return (x,y)
+
+
+    def turn(self):
+        """
+        creates intends in which our vehicles will move
+        :return:
+        """
+        pass
+
+    def simlulate(self):
+        """
+        simulation of a turn
+        :return:
+        """
+        pass
+
+
+if __name__ == "__main__":
+    e=Ecosystem(3,3,100,2)
+
+    print(e)
